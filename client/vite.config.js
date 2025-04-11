90fe6e3dc60ec89de47fad4c6ac32fb3c87f51d6
esencia.stv <esencia.stv@gmail.com>
2025-04-11 15:44:39 -0300

    fix error in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import fs from "fs";
 
-const isDev = process.env.NODE_ENV === 'development' || import.meta.env.MODE === 'development'
-console.log(isDev);
+const isDev = process.env.NODE_ENV === 'development' 
 
 export default defineConfig({
   plugins: [
